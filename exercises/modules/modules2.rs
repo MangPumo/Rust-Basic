738884458f4adb1b38bb9adb38aff6f18d673468
MangPumo <202205566304@smail.xtu.edu.cn>
2025-01-22 23:15:02 +0800

    update

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -1,16 +1,9 @@
 // modules2.rs
-//
-// You can bring module paths into scopes and provide new names for them with
-// the 'use' and 'as' keywords. Fix these 'use' statements to make the code
-// compile.
-//
-// Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
-// hint.
-
-// I AM NOT DONE
+// You can bring module paths into scopes and provide new names for them with the
+// 'use' and 'as' keywords. Fix these 'use' statements to make the code compile.
+// Execute `rustlings hint modules2` or use the `hint` watch subcommand for a hint.
 
 mod delicious_snacks {
-    // TODO: Fix these use statements
     pub use self::fruits::PEAR as pear;
     pub use self::veggies::CUCUMBER as cucumber;
 
@@ -28,7 +21,7 @@ mod delicious_snacks {
 fn main() {
     println!(
         "favorite snacks: {} and {}",
-        delicious_snacks::fruit,
-        delicious_snacks::veggie
+        delicious_snacks::pear,
+        delicious_snacks::cucumber
     );
 }
